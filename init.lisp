eb05877aafff993ef2473656e84b6e028d6b5536
mwgkgk <mwgkgk@gmail.com>
2022-10-29 13:10:21 +0300

    Add everything

---
+++ init.lisp
@@ -0,0 +1,54 @@
+(in-package :lem-user)
+
+;;; Enable vi mode
+;;; See ~/.roswell/local-projects/cxxxr/lem/modes/vi-mode/README.md/
+(lem-vi-mode:vi-mode)
+
+;;; *
+;;; * Keybindings / Vim
+;;; *
+
+(define-key lem-vi-mode:*command-keymap* "Space q" 'vi-quit)
+
+(define-key lem-vi-mode:*command-keymap* "Space h" 'window-move-left)
+(define-key lem-vi-mode:*command-keymap* "Space l" 'window-move-right)
+(define-key lem-vi-mode:*command-keymap* "Space k" 'window-move-up)
+(define-key lem-vi-mode:*command-keymap* "Space j" 'window-move-down)
+
+(define-key lem-vi-mode:*command-keymap* "Space w" 'save-current-buffer)
+(define-key lem-vi-mode:*command-keymap* "Space e" 'find-file)
+
+(define-key lem-vi-mode:*command-keymap* "Space n" 'split-active-window-vertically)
+(define-key lem-vi-mode:*command-keymap* "Space N" 'split-active-window-horizontally)
+
+;;; TODO move upstream
+(define-key lem-vi-mode:*command-keymap* "C-d" 'next-page)
+
+;;; *
+;;; * Keybindings / Lisp
+;;; *
+
+(define-key lem-vi-mode:*command-keymap* "X" 'lem-lisp-mode:lisp-compile-defun)
+(define-key lem-vi-mode:*command-keymap* "Space x" 'lem-lisp-mode:lisp-compile-and-load-file)
+
+;;; *
+;;; * Colors
+;;; *
+
+(define-color-theme "zone" ()
+  (:display-background-mode :dark)
+  ;; (:foreground "#FFFFFF")
+  ;; (:background "#000000")
+  (:inactive-window-background nil)
+  (region :foreground nil :background "blue")
+  (modeline :background "#404040" :foreground "white")
+  (modeline-inactive :background "#303030" :foreground "gray")
+  (syntax-string-attribute :foreground "light salmon")
+  (syntax-comment-attribute :foreground "chocolate1")
+  (syntax-keyword-attribute :foreground "cyan1")
+  (syntax-constant-attribute :foreground "LightSteelBlue")
+  (syntax-function-name-attribute :foreground "LightSkyBlue")
+  (syntax-variable-attribute :foreground "LightGoldenrod")
+  (syntax-type-attribute :foreground "PaleGreen"))
+
+(lem::apply-theme (lem::find-color-theme "zone"))
